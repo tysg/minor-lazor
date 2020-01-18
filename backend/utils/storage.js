b4ffe49fd9056d8ee6d438cd7b61e171df853000
Branson <branson.ng@u.nus.edu>
2020-01-18 20:36:43 +0800

    temp

--- backend/utils/storage.js
+++ backend/utils/storage.js
@@ -5,7 +5,7 @@ const accepted = ["image/png", "image/jpg", "image/jpeg"];
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./uploads");
-  }
+  },
   filename: (req, file, cb) => {
     cb(null, `${file.fieldname}_${+new Date()}.jpg`);
   }
