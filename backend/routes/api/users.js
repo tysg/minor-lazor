c357a51f54849bf9d7e6e5cbbc73fef0fa2daec8
dingyuchen <dingyuchen@gmail.com>
2020-01-18 17:37:27 +0800

    Create endpoints for each step of flow

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var request = require("request");
 
 const subscriptionKey = process.env.API_KEY1;
 
@@ -9,8 +10,27 @@ router.get("/", function(req, res, next) {
 });
 
 router.post("/create", function(req, res, next) {
-  const faceIds = req.body;
-  res.send("created a user");
+  const person = req.body;
+  // const faceIds = res.send("created a user");
+  // get userId from mongo
+  // add person to personGroup
+});
+
+router.post("/:userId/upload", (req, res, next) => {
+  const { userID } = req.params;
+  const options = {
+    uri: personAddFaceEndpoint,
+    qs: params,
+    headers: {
+      "Content-Type": "application/json",
+      "Ocp-Apim-Subscription-Key": subscriptionKey
+    }
+  };
+  // add face to person
+});
+
+router.get("/train", (req, res, next) => {
+  // train person group
 });
 
 module.exports = router;
